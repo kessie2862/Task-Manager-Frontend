e836d133c8658d2cf1a3e6218d703d780598edcc
kessie <kayp3477@gmail.com>
2024-07-17 18:15:19 +0000

    Add response interceptor for token refresh on 401 error

--- src/services/api.js
+++ src/services/api.js
@@ -17,3 +17,28 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const token = getToken().refresh;
+        const response = await api.post('/api/token/refresh/', {
+          refresh: token,
+        });
+        const newToken = response.data.access;
+        setToken({ access: newToken, refresh: token });
+        axios.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
+        return api(originalRequest);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
