844e9ed46bf9799f26b8d64f5d02ca105c0e48f6
kessie <kayp3477@gmail.com>
2024-07-17 18:14:22 +0000

    Add request interceptor to set Authorization header

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,16 @@ import { getToken, setToken } from './auth';
 const api = axios.create({
   baseURL: 'http://localhost:8000',
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getToken().access;
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
